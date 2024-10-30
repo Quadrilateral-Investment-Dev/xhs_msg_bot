da9ae26acd1dfad0e6e5f925e5506d1562b18e08
Kenneth Liu <kennethliu12138@gmail.com>
2024-10-30 15:23:35 +1100

    working

--- app/src/main/java/com/example/xiaohongshuautomation/DataReader.java
+++ app/src/main/java/com/example/xiaohongshuautomation/DataReader.java
@@ -9,10 +9,7 @@ import org.apache.commons.csv.CSVRecord;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class DataReader {
